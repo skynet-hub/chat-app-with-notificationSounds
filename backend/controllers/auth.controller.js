14d48778071b4ba34848314917b3cbbe57a804fc
Magobo <malesaojhb024@student.wethinkcode.co.za>
2026-01-21 18:12:20 +0200

    logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -53,10 +53,41 @@ export const signup = async (req, res) => {
 }
 
 
-export const login = (req, res) => {
-    res.send("Login Route")
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            gender: user.gender,
+            profilePic: user.profilePic
+        })
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        res.status(500).json({error: error.message})
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("Logout Route")
+    try {
+        res.cookie("jwt", "", {
+            maxAge: 0
+        })
+
+        res.status(200).json({message: "Logged out successfully"})
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
 }
