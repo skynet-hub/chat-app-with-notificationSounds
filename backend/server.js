7e258044406f30772f334807d143bffa606d724d
Magobo <malesaojhb024@student.wethinkcode.co.za>
2026-01-21 18:16:11 +0200

    skeleton message routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connectToDB from './db/connectToDB.js'
 
 const app = express()
@@ -12,6 +13,7 @@ dotenv.config()
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 
 app.listen(PORT, () => {
